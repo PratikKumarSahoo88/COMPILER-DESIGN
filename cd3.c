45206db842b0fa34d39d04b087c23683c0909463
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2026-02-17 22:42:27 +0530

    exp 3

---
+++ cd3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#define MATCH printf("\n The test match the regular expression");
+#define NOTMATCH printf("\n The test match does not the regular expression")
+char reg[20],text[20];
+int main(){
+    int i,run,then R=0;
+    char ans;
+    do{
+        printf("\n enter the RE :\n");
+        scanf("%[^\n]s",reg);
+        for(run=0;reg[run]!='\0',rlen++);
+        printf("\n Enter the text\n");
+        scanf("%[^\n]s",text);
+        
+
+    }
+}
