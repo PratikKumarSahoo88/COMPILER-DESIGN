e840833653d8e0daea270d201fc324dbdad33f5b
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2026-03-11 12:03:25 +0530

    Implement predictive parsing logic in C

--- Predictive parser.c
+++ Predictive parser.c
@@ -323,3 +323,122 @@ error:
     printf("\nString Rejected\n");
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+char stack[50], input[50];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+void pop() {
+    if (top >= 0) top--;
+}
+
+void display_stack() {
+    for (int j = 0; j <= top; j++) printf("%c", stack[j]);
+}
+
+int main() {
+    int i = 0;
+    char x;
+    printf("Enter the input string (e.g., i+i*i$): ");
+    scanf("%s", input);
+
+    push('$');
+    push('E');
+
+    printf("\nStack\t\tInput\t\tAction\n");
+    printf("----------------------------------------------\n");
+
+    while (top >= 0) {
+        x = stack[top];
+        
+        display_stack();
+        printf("\t\t%s\t\t", &input[i]);
+
+        if (x == input[i]) {
+            pop();
+            i++;
+            printf("Match %c\n", x);
+            if (x == '$') {
+                printf("\nString Accepted\n");
+                return 0;
+            }
+        } 
+        else if (x == '#') { // # represents Epsilon (empty string)
+            pop();
+            printf("Pop Epsilon\n");
+        }
+        else {
+            pop(); // Pop the non-terminal to replace it with production
+            switch (x) {
+                case 'E':
+                    if (input[i] == 'i' || input[i] == '(') {
+                        push('e'); // 'e' represents E'
+                        push('T');
+                        printf("E -> Te\n");
+                    } else goto error;
+                    break;
+                case 'e': // E'
+                    if (input[i] == '+') {
+                        push('e');
+                        push('T');
+                        push('+');
+                        printf("E' -> +Te\n");
+                    } else { // E' -> epsilon
+                        push('#');
+                        printf("E' -> #\n");
+                    }
+                    break;
+                case 'T':
+                    if (input[i] == 'i' || input[i] == '(') {
+                        push('t'); // 't' represents T'
+                        push('F');
+                        printf("T -> Ft\n");
+                    } else goto error;
+                    break;
+                case 't': // T'
+                    if (input[i] == '*') {
+                        push('t');
+                        push('F');
+                        push('*');
+                        printf("T' -> *Ft\n");
+                    } else { // T' -> epsilon
+                        push('#');
+                        printf("T' -> #\n");
+                    }
+                    break;
+                case 'F':
+                    if (input[i] == 'i') {
+                        push('i');
+                        printf("F -> i\n");
+                    } else if (input[i] == '(') {
+                        push(')');
+                        push('E');
+                        push('(');
+                        printf("F -> (E)\n");
+                    } else goto error;
+                    break;
+                default:
+                    goto error;
+            }
+        }
+    }
+
+error:
+    printf("\nString Rejected\n");
+    return 0;
+}
